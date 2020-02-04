6b9c773ebade7ba2401e8e9baff820a828f4f815
thanhle <thanhle12345789@gmail.com>
2020-02-04 14:56:01 +0700

    first init

---
+++ index.js
@@ -0,0 +1,14 @@
+const sayHello = () => {
+    console.log('hello 1');
+};
+
+sayHello();
+
+function sayHello2 () {
+    console.log('hello 2'
+    )
+}
+
+sayHello2();
+/// co git roi nhe
+/// gio thu cai git nhe
